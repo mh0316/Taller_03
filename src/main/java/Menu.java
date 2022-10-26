2e0bd2fe8f9118d699ab5b0cd30783743223e00b
Marcelo <m.henriquez11@ufromail.cl>
2022-10-26 10:21:21 -0300

    Tercer commit

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-    private List<Mesero> mesero;
+    Pedido pedido;
 
     public void mostrarMenu(){
         System.out.println("""
@@ -11,38 +11,7 @@ public class Menu {
                 1. Mostrar pedido.
                 2. Modificar pedido.
                 3. Salir.""");
-        int opcion = ingresar();
-    }
-
-    private void opcionesMenu(int opcion){
-        switch (opcion){
-            case 1:
-                mostrarPedido();
-            case 2:
-                modificarPedido();
-            case 3:
-                terminarPrograma();
-        }
-    }
-
-    private void mostrarPedido() {
-        char[][] pedidos = new char[1][1];
-        for (char[] pedido : pedidos) {
-            System.out.println(Arrays.toString(pedido));
-        }
-    }
-
-    public void modificarPedido() {
-        int numeroPedidos = 0;
-        for(Mesero mesero : this.mesero) {
-            if(mesero.getMesero() != numeroPedidos) {
-                this.mesero.add(mesero);
-            }
-        }
-    }
-
-    private void terminarPrograma(){
-        System.out.println("Programa terminado. Gracias.");
+        ingresar();
     }
 
     public static int ingresar() {
